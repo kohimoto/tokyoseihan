24dc9762dbf6e4a4b2b3c244c954364c2e0698b4
kohimoto <kohimotoj@gmail.com>
2018-11-26 15:29:17 +0900

    index

--- app/public/index.php
+++ app/public/index.php
@@ -1,80 +1,74 @@
 <?php
 include("./header-top.php");
 ?>
-<div id="main-img">
-	<div class="main-box row">
-		<img src="/images/main.jpg" alt="東京製版">
-		<section id="section05" class="demo">
-	  <a href="#mission"><span></span></a>
-		</section>
-	</div>
-</div>
-<div id="mission" class="row">
-  <h2>ミッション</h2>
-	<h3><span>“設計” ＝ Design</span><br>
-われわれは事業ブランドを設計し創りあげる会社。<br>
-企業がもつ“ロイヤル顧客”という資産で、<br>
-企業の収益基盤へつなぐBrand を創ります。<br>
-そのためにエクスターナル領域にとどまらず、<br>
-インターナル領域もふくめたBrand設計を行います。<br>
-</h3>
-</div>
-<div id="products" class="row gray">
-  <h2>ハンコの種類</h2>
-	<h3>“DATAマネージメント”をコアエンジンにし、<br>
-External＆internalのブランディングを実現します。</h3>
-	<div class="plan_02">
-		<div class="circle">
-			<img src="images/business_01.png" alt="External & Internal Branding">
-		</div>
-		<div class="line">
-			<svg class="pc"><use xlink:href="#line"></use></svg>
-			<svg  viewBox="0 0 360.824 111.883" width="360.824" height="111.883" class="sp"><use xlink:href="#line_sp"></use></svg>
+<div class="wrap">
+	<div id="main">
+		<div class="main-box">
+			<img src="/images/main.jpg" alt="東京製版">
 		</div>
 	</div>
-</div>
-<div id="news" class="row">
-  <h2>お知らせ</h2>
-	<div class="sd_04">
-		<div class="box">
-			<p>会社名</p>
-				<div class="date">
-					<h5>株式会社 澤田設計事務所</h5>
-				</div>
-		</div>
-		<div class="box">
-			<p>本社所在地</p>
-				<div class="date">
-					<h5>大阪府大阪市北区中之島2-2-7</h5>
-				</div>
-		</div>
-		<div class="box">
-			<p>資本金</p>
-				<div class="date">
-					<h5>5,000万円</h5>
-				</div>
-		</div>
-		<div class="box">
-			<p>設立日</p>
-				<div class="date">
-					<h5>2018年9月3日</h5>
-				</div>
-		</div>
-		<div class="box officer">
-			<p>役員</p>
-				<div class="date">
-					<h5>代表取締役社長  澤田善郎<br>
-							取締役（非常勤）沖林義樹<br>
-							取締役（非常勤）武田和雄<br>
-							監査役（非常勤）西田雅彦<br>
-					</h5>
-				</div>
+	<div id="mission" class="row">
+	  <h2>ミッション</h2>
+		<h3><span>“設計” ＝ Design</span><br>
+	われわれは事業ブランドを設計し創りあげる会社。<br>
+	企業がもつ“ロイヤル顧客”という資産で、<br>
+	企業の収益基盤へつなぐBrand を創ります。<br>
+	そのためにエクスターナル領域にとどまらず、<br>
+	インターナル領域もふくめたBrand設計を行います。<br>
+	</h3>
+	</div>
+	<div id="products" class="row gray">
+	  <h2>ハンコの種類</h2>
+		<h3>“DATAマネージメント”をコアエンジンにし、<br>
+	External＆internalのブランディングを実現します。</h3>
+		<div class="plan_02">
+			<div class="circle">
+				<img src="images/business_01.png" alt="External & Internal Branding">
+			</div>
+			<div class="line">
+				<svg class="pc"><use xlink:href="#line"></use></svg>
+				<svg  viewBox="0 0 360.824 111.883" width="360.824" height="111.883" class="sp"><use xlink:href="#line_sp"></use></svg>
+			</div>
 		</div>
 	</div>
-</div>
-<div id="ask" class="row">
-	<div class="btn_area">
-		<a class="button" href="mailto:&#105;&#110;&#102;&#111;&#64;&#115;&#97;&#119;&#97;&#100;&#97;&#45;&#115;&#101;&#107;&#107;&#101;&#105;&#46;&#99;&#111;&#46;&#106;&#112;">お問合せはコチラ&nbsp;&#8811;</a>
+	<div id="news" class="row">
+	  <h2>お知らせ</h2>
+		<div class="sd_04">
+			<div class="box">
+				<p>会社名</p>
+					<div class="date">
+						<h5>株式会社 澤田設計事務所</h5>
+					</div>
+			</div>
+			<div class="box">
+				<p>本社所在地</p>
+					<div class="date">
+						<h5>大阪府大阪市北区中之島2-2-7</h5>
+					</div>
+			</div>
+			<div class="box">
+				<p>資本金</p>
+					<div class="date">
+						<h5>5,000万円</h5>
+					</div>
+			</div>
+			<div class="box">
+				<p>設立日</p>
+					<div class="date">
+						<h5>2018年9月3日</h5>
+					</div>
+			</div>
+			<div class="box officer">
+				<p>役員</p>
+					<div class="date">
+						<h5>代表取締役社長  澤田善郎<br>
+								取締役（非常勤）沖林義樹<br>
+								取締役（非常勤）武田和雄<br>
+								監査役（非常勤）西田雅彦<br>
+						</h5>
+					</div>
+			</div>
+		</div>
 	</div>
 </div>
 <?php
