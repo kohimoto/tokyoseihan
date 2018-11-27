9d7f5d5d7b497d9bf2422c899a4d9723ee17b245
kohimoto <kohimotoj@gmail.com>
2018-11-27 14:59:45 +0900

    news

--- app/public/index.php
+++ app/public/index.php
@@ -41,7 +41,7 @@ include("./header.php");
 			<div class="main-box">
 				<h4>ダミーダミー</h4>
 				<p>ハンコの歴史は意外に古く...東京製版ハンコゴム印浸透印東京製版ハンコゴム印浸透印
-...用途によって様々なハンコがあります。</p>
+...用途によって様々なハンコがあります。(ダミーテキストダミーテキストダミーテキスト)</p>
 				<div class="box">
 					<h4>ゴム印</h4>
 					<ul>
@@ -64,41 +64,35 @@ include("./header.php");
 		</div>
 	</div>
 	<div id="news">
+		<div class="main-img">
+			<img src="/images/yoko100_dmy.png" alt="">
+		</div>
+		<div class="container">
+			<div class="main-txt">
 	  <h2><span>NEWS</span><br>お知らせ</h2>
-		<div class="sd_04">
-			<div class="box">
-				<p>会社名</p>
-					<div class="date">
-						<h5>株式会社 澤田設計事務所</h5>
-					</div>
 			</div>
-			<div class="box">
-				<p>本社所在地</p>
-					<div class="date">
-						<h5>大阪府大阪市北区中之島2-2-7</h5>
-					</div>
-			</div>
-			<div class="box">
-				<p>資本金</p>
-					<div class="date">
-						<h5>5,000万円</h5>
-					</div>
-			</div>
-			<div class="box">
-				<p>設立日</p>
-					<div class="date">
-						<h5>2018年9月3日</h5>
-					</div>
-			</div>
-			<div class="box officer">
-				<p>役員</p>
-					<div class="date">
-						<h5>代表取締役社長  澤田善郎<br>
-								取締役（非常勤）沖林義樹<br>
-								取締役（非常勤）武田和雄<br>
-								監査役（非常勤）西田雅彦<br>
-						</h5>
-					</div>
+			<div class="main-box">
+				<h4>ダミーダミー</h4>
+				<p>ハンコの歴史は意外に古く...東京製版ハンコゴム印浸透印東京製版ハンコゴム印浸透印
+...用途によって様々なハンコがあります。(ダミーテキストダミーテキストダミーテキスト)</p>
+				<div class="box">
+					<h4>ゴム印</h4>
+					<ul>
+						<li><a href="#">のべ台</a></li>
+						<li><a href="#">ニギリ台</a></li>
+						<li><a href="#">のべ台</a></li>
+						<li><a href="#">あああああ</a></li>
+						<li><a href="#">のべ台</a></li>
+						<li><a href="#">いいい</a></li>
+						<li><a href="#">のべ台</a></li>
+					</ul>
+				</div>
+				<div class="box">
+					<h4>ゴム印</h4>
+					<ul>
+						<li><a href="#">のべ台</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
