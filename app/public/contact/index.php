1942e0e2f2859995cab8e07726b72db4000b5b45
kohimoto1! <kohimotoj@gmail.com>
2018-11-28 10:49:00 +0900

    change

--- app/public/contact/index.php
+++ app/public/contact/index.php
@@ -8,7 +8,7 @@ include("../header.php");
 	<div id="technology">
 		<div class="container">
 		<div class="main-txt">
-	  	<h2><span>TECHNOLOGY & QUALITY</span><br>技術と品質</h2>
+	  	<h2><span>CONTACT US</span><br>お問い合わせ</h2>
 		</div>
 		<div class="main-box">
 			<p>技術革新が進み利便性が向上し、これまでにない常識が次々と生み出される現代。<br>
@@ -18,57 +18,6 @@ include("../header.php");
 		</div>
 		</div>
 	</div>
-  <div class="img-box">
-    <div class="row">
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-txt col-sm-6">
-        <h3>ゴム印<br>製造過程</h3>
-        <span></span>
-        <p>印面が天然ゴムによる赤ゴムと、<br>耐油性のある黒ゴム素材のもの、<br>またはゴムではない<br>半透明の合成樹脂を使ったスタンプ。</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-    </div>
-  </div>
-  <div class="mov-box">
-    <img src="/images/yoko100_dmy.png" alt="">
-  </div>
-  <div class="img-box">
-    <div class="row">
-      <div class="inner-txt col-sm-6">
-        <h3>ハンコ<br>製造過程</h3>
-        <span></span>
-        <p>印面が天然ゴムによる赤ゴムと、<br>耐油性のある黒ゴム素材のもの、<br>またはゴムではない<br>半透明の合成樹脂を使ったスタンプ。</p>
-      </div>
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-    </div>
-    <div class="row">
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-    </div>
-  </div>
-  <div class="mov-box">
-    <img src="/images/yoko100_dmy.png" alt="">
-  </div>
-  <div class="img-box">
-    <div class="row">
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-txt col-sm-6">
-        <h3>浸透印<br>製造過程</h3>
-        <span></span>
-        <p>印面が天然ゴムによる赤ゴムと、<br>耐油性のある黒ゴム素材のもの、<br>またはゴムではない<br>半透明の合成樹脂を使ったスタンプ。</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
-    </div>
-  </div>
-  <div class="mov-box last-box">
-    <img src="/images/yoko100_dmy.png" alt="">
-  </div>
 
 </div>
 <?php
