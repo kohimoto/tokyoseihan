dd66cce16bd8e43709d3d9972aee8b6d510d0244
kohimoto1! <kohimotoj@gmail.com>
2018-11-28 16:20:26 +0900

    tech sp

--- app/public/technology/index.php
+++ app/public/technology/index.php
@@ -3,7 +3,7 @@ include("../header.php");
 ?>
 <div class="wrap tech other_page">
 	<div id="main">
-    <img src="/images/yoko100_dmy.png" alt="技術と品質">
+    <img src="/images/yoko100_dmy.png" alt="col-sm-6">
 	</div>
 	<div id="technology">
 		<div class="container">
@@ -20,16 +20,16 @@ include("../header.php");
 	</div>
   <div class="img-box">
     <div class="row">
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-txt col-xs-6 fadein">
+      <div class="inner-txt col-sm-6 change-box fadein">
         <h3>ゴム印<br>製造過程</h3>
         <span></span>
         <p>印面が天然ゴムによる赤ゴムと、<br>耐油性のある黒ゴム素材のもの、<br>またはゴムではない<br>半透明の合成樹脂を使ったスタンプ。</p>
       </div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
     </div>
     <div class="row">
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
     </div>
   </div>
   <div class="mov-box">
@@ -37,16 +37,16 @@ include("../header.php");
   </div>
   <div class="img-box">
     <div class="row">
-      <div class="inner-txt col-xs-6">
+      <div class="inner-txt col-sm-6">
         <h3>ハンコ<br>製造過程</h3>
         <span></span>
         <p>印面が天然ゴムによる赤ゴムと、<br>耐油性のある黒ゴム素材のもの、<br>またはゴムではない<br>半透明の合成樹脂を使ったスタンプ。</p>
       </div>
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
     </div>
     <div class="row">
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
     </div>
   </div>
   <div class="mov-box">
@@ -54,16 +54,16 @@ include("../header.php");
   </div>
   <div class="img-box">
     <div class="row">
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-txt col-xs-6">
+      <div class="inner-txt col-sm-6 change-box">
         <h3>浸透印<br>製造過程</h3>
         <span></span>
         <p>印面が天然ゴムによる赤ゴムと、<br>耐油性のある黒ゴム素材のもの、<br>またはゴムではない<br>半透明の合成樹脂を使ったスタンプ。</p>
       </div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
     </div>
     <div class="row">
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
-      <div class="inner-img col-xs-6"><img src="/images/gijyutu_dmy.png"></div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
+      <div class="inner-img col-sm-6"><img src="/images/gijyutu_dmy.png"></div>
     </div>
   </div>
   <div class="mov-box last-box">
